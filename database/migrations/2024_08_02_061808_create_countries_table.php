3dacff3bb92864082ae4e8bc6a61d46682d38a94
SHARAFAHMED3 <sharafahmed0303@gmail.com>
2026-05-15 12:07:31 +0530

    completed

--- database/migrations/2024_08_02_061808_create_countries_table.php
+++ database/migrations/2024_08_02_061808_create_countries_table.php
@@ -19,8 +19,6 @@ return new class extends Migration
             $table->string('countries_name');
             $table->string('phonecode');
 
-            // Indexes
-            $table->primary(['id']);
             $table->index(['countries_code']);
         });
 
